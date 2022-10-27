aafe11f48df50e5544eed2a1395ef9457d88c773
FerEquis <fcastilloaguirre@gmail.com>
2022-10-27 16:45:33 -0300

    div en el texto

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function validarForm() {
     let x = document.forms["myform"]["email"].value;
     if (x == "") {
-      alert("Necesitamos al menos un email de contacto");
+      alert("Necesitamos un email de contacto");
       return false;
     }
   }
