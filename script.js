a6c5ef3faaaa9fb1cd1778d8a16e2fc3a91b0229
FerEquis <fcastilloaguirre@gmail.com>
2022-10-29 15:59:24 -0300

    cambios

--- script.js
+++ script.js
@@ -5,3 +5,44 @@ function validarForm() {
       return false;
     }
   }
+
+
+function traer() {
+    fetch('https://randomuser.me/api')
+        .then(res => res.json())
+        .then(res => {
+             console.log(res)
+             console.log(res.results[0].email)
+             contenido.innerHTML= `
+               <img src="${res.results[0].picture.large}
+                 " width="200px" 
+                 class="img-fluid rounded-circle">
+               <p>Nombre: ${res.results[0].name.first}</p>
+               <p>Mail: ${res.results[0].email}</p>`
+          })
+       }
+
+
+//const{ createApp } = Vue
+//  createApp({
+//      data(){
+//        return{
+//          mensaje: "Hola Mundo con Vue",
+//          curso: "Codo a Codo"
+//        }
+//      }
+//  }).mount("#app")
+
+//fetch('https://api.coindesk.com/v1/bpi/currentprice.json') 
+//  .then(response => response.json()) 
+//  .then(data => console.log(data))
+
+
+//var contenido = document.querySelector("#contenido");
+//function traer() {
+//  fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+//    .then(data => data.text())
+//    .then(data => {
+//      contenido.innerHTML= "${data}"})
+//    }
+
